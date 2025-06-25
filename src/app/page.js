c178171bfbfe0b3a7b6aa740e8809e8c29d1371b
shahnilsharma <shahnil.sharma402@gmail.com>
2025-06-25 20:31:44 +0500

    globalcomps

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Button from '@/components/GlobalComponents/Button'
+import InputField from '@/components/GlobalComponents/InputField'
+import AuthPattern from '@/components/UI/AuthPattern'
+
 
 export default function page() {
   return (
     <>
   <div>
-    theme 
-  </div>
+    theme z
+    <InputField/>
+<Button src={"searchBox"} text={"hello"} />
+<Button text={"hello"} />
+<AuthPattern/>
+  
+    </div>
     </>
   )
 }
