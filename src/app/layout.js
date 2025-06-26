0a3a28e15f1866c382a6448449ca3ec9e43fa86e
SherazNadeem07 <msheraznadeem07@gmail.com>
2025-06-26 13:30:50 +0500

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/Redux/Provider/provider";
-import ThemeWrapper from "./components/globalcomponents/ThemeWrapper"; // 👈 add this line
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,12 +19,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <Providers>
-      <ThemeWrapper>
-        <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          {children}
-        </div>
-      </ThemeWrapper>
-    </Providers>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+        {children}
+        </Providers>
+      </body>
+    </html>
   );
 }
